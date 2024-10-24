6ea30e930b71ea6a4ee54b5392476e2e4bc93f6c
happycho <zanda33@naver.com>
2024-10-24 11:09:25 +0900

    BUGFIX: 이미지 오류 개선
    next 버전 및 react 버전 수정

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/blog",
-  assetPrefix: "/blog/",
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- apps/blog/pages/index.tsx
+++ apps/blog/pages/index.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
@@ -14,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main}`}>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -28,7 +26,7 @@ export default function Home() {
             >
               By{" "}
               <Image
-                src="/blog/vercel.svg"
+                src="/vercel.svg"
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
                 width={100}
@@ -42,7 +40,7 @@ export default function Home() {
         <div className={styles.center}>
           <Image
             className={styles.logo}
-            src="/blog/next.svg"
+            src="/next.svg"
             alt="Next.js Logo"
             width={180}
             height={37}
